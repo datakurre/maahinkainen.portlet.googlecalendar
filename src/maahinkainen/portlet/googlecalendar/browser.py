279bf88a13b7f70b43a1b7070378d842f08fe51f
Asko Soukka <asko.soukka@iki.fi>
2012-08-05 01:06:10 +0300

    Fixed unicode-issues with displaying event title or description.

--- src/maahinkainen/portlet/googlecalendar/browser.py
+++ src/maahinkainen/portlet/googlecalendar/browser.py
@@ -10,7 +10,7 @@ from Products.Five.browser import BrowserView
 
 from maahinkainen.portlet.googlecalendar.utility import EVENTS
 
-LINK_REGEXP = re.compile((r"(\b(http|https)://" 
+LINK_REGEXP = re.compile((r"(\b(http|https)://"
                           r"([-A-Za-z0-9+&@#/%?=~_()|!:,.;]*"
                           r"[-A-Za-z0-9+&@#/%=~_()|]))"))
 
@@ -18,9 +18,9 @@ LINK_REGEXP = re.compile((r"(\b(http|https)://"
 class Month(BrowserView):
     """View a month, via ../context/@@calendar-month/calendar-id
     """
-    
+
     implements(IPublishTraverse)
-    
+
     def __init__(self, context, request):
         super(Month, self).__init__(context, request)
         self.calendar_id = None
@@ -37,17 +37,20 @@ class Month(BrowserView):
 class Event(BrowserView):
     """View an event, via ../context/@@calendar-event/calendar-id/event-id
     """
-    
+
     implements(IPublishTraverse)
-    
+
     def __init__(self, context, request):
         super(Event, self).__init__(context, request)
         self.calendar = {}
         self.timezone = self.request.get("ctz", "Europe/Helsinki")
         self.event = None
 
+    def get_title(self):
+        return unicode(self.event.get_title(), "utf-8")
+
     def get_description(self):
-        s = self.event.get_description()
+        s = unicode(self.event.get_description(), "utf-8")
         for match in LINK_REGEXP.findall(s):
             s = s.replace(match[0], u'<a href="%s">%s</a>' % (match[0], match[0]))
         return s
