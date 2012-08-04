b5c68cfd497916fb3acbecbb72e812e47886aedd
Asko Soukka <asko.soukka@iki.fi>
2012-08-04 17:48:55 +0300

    Moved code under 'src'.
    --HG--
    rename : maahinkainen/__init__.py => src/maahinkainen/__init__.py
    rename : maahinkainen/portlet/__init__.py => src/maahinkainen/portlet/__init__.py
    rename : maahinkainen/portlet/googlecalendar/__init__.py => src/maahinkainen/portlet/googlecalendar/__init__.py
    rename : maahinkainen/portlet/googlecalendar/agenda.pt => src/maahinkainen/portlet/googlecalendar/agenda.pt
    rename : maahinkainen/portlet/googlecalendar/browser.py => src/maahinkainen/portlet/googlecalendar/browser.py
    rename : maahinkainen/portlet/googlecalendar/configure.zcml => src/maahinkainen/portlet/googlecalendar/configure.zcml
    rename : maahinkainen/portlet/googlecalendar/event.pt => src/maahinkainen/portlet/googlecalendar/event.pt
    rename : maahinkainen/portlet/googlecalendar/events.pt => src/maahinkainen/portlet/googlecalendar/events.pt
    rename : maahinkainen/portlet/googlecalendar/googlecalendar.py => src/maahinkainen/portlet/googlecalendar/googlecalendar.py
    rename : maahinkainen/portlet/googlecalendar/interfaces.py => src/maahinkainen/portlet/googlecalendar/interfaces.py
    rename : maahinkainen/portlet/googlecalendar/locales/en/LC_MESSAGES/maahinkainen.portlet.googlecalendar.po => src/maahinkainen/portlet/googlecalendar/locales/en/LC_MESSAGES/maahinkainen.portlet.googlecalendar.po
    rename : maahinkainen/portlet/googlecalendar/locales/fi/LC_MESSAGES/maahinkainen.portlet.googlecalendar.po => src/maahinkainen/portlet/googlecalendar/locales/fi/LC_MESSAGES/maahinkainen.portlet.googlecalendar.po
    rename : maahinkainen/portlet/googlecalendar/locales/maahinkainen.portlet.googlecalendar.pot => src/maahinkainen/portlet/googlecalendar/locales/maahinkainen.portlet.googlecalendar.pot
    rename : maahinkainen/portlet/googlecalendar/month.pt => src/maahinkainen/portlet/googlecalendar/month.pt
    rename : maahinkainen/portlet/googlecalendar/portlet.py => src/maahinkainen/portlet/googlecalendar/portlet.py
    rename : maahinkainen/portlet/googlecalendar/profiles/default/jsregistry.xml => src/maahinkainen/portlet/googlecalendar/profiles/default/jsregistry.xml
    rename : maahinkainen/portlet/googlecalendar/profiles/default/metadata.xml => src/maahinkainen/portlet/googlecalendar/profiles/default/metadata.xml
    rename : maahinkainen/portlet/googlecalendar/profiles/default/portlets.xml => src/maahinkainen/portlet/googlecalendar/profiles/default/portlets.xml
    rename : maahinkainen/portlet/googlecalendar/static/overlay.js => src/maahinkainen/portlet/googlecalendar/static/overlay.js
    rename : maahinkainen/portlet/googlecalendar/utility.py => src/maahinkainen/portlet/googlecalendar/utility.py

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
 from setuptools import setup, find_packages
 
-version = '1.0b2'
+version = "1.0.0"
 
-setup(name='maahinkainen.portlet.googlecalendar',
+setup(name="maahinkainen.portlet.googlecalendar",
       version=version,
       description="Simple Google Calendar events portlet.",
       long_description=open("README.txt").read() + "\n" +
                        open("HISTORY.txt").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='Asko Soukka',
-      author_email='asko.soukka@iki.fi',
-      url='http://pypi.python.org/pypi/maahinkainen.portlet.googlecalendar/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['maahinkainen', 'maahinkainen.portlet'],
+           "Programming Language :: Python",
+      ],
+      keywords="",
+      author="Asko Soukka",
+      author_email="asko.soukka@iki.fi",
+      url=("https://github.com/datakurre/"
+           "maahinkainen.portlet.googlecalendar/"),
+      license="GPL",
+      packages=find_packages("src", exclude=["ez_setup"]),
+      package_dir={"": "src"},
+      namespace_packages=["maahinkainen", "maahinkainen.portlet"],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          "setuptools",
           # -*- Extra requirements: -*-'
           'PyXML', # for googlecalendar.py
           'icalendar', # for googlecalendar.py
           'gdata', # for googlecalendar.py and me
       ],
+      extras_require={
+           "test": ["plone.app.testing", "corejet.core"],
+      },
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
-)
+      """
+      )
